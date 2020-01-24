6dc0730e6c6fe09e197cedcc3da6c8f965591769
James Hagood <cardfaux@tutanota.com>
2020-01-24 10:03:17 -0600

    added the time complexity for array methods

--- Main.js
+++ Main.js
@@ -3,17 +3,17 @@ const numbers = [1, 2, 3, 4, 5];
 // Variable array is somewhere in memory and the computer knows it. Stored in the RAM random access memory.
 // When I do array[2], i'm telling the computer, hey go to the array and grab the 3rd item from where the array is stored.
 
-//push
-strings.push('e');
+//push adds to the end of the array
+strings.push('e'); // 0(1) operation -- Time Complexity
 
-//pop
+//pop remove the last item in an array  -- 0(1) -- Time Complexity
 strings.pop();
 strings.pop();
 
-//unshift
-strings.unshift('x');
+//unshift adds item at the beginning of an array TimeComplexity is 0(n) time is based on the size of the array because all the indexes have to shift
+strings.unshift('x'); // Causing a loop
 
-//splice
+//splice this adds alienat the index of 2 and the 0 says don't delete anything -- Time Complexity 0(n) indexes have to be looped to change
 strings.splice(2, 0, 'alien');
 
 console.log(strings);
